1ab72783084618f99f73c89baa5d3367ec963458
autumn-ragland <raglanda998@gmail.com>
2020-09-08 16:29:32 -0500

    add in browser error

--- BookAuthors/Controllers/BookAuthorController.cs
+++ BookAuthors/Controllers/BookAuthorController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using BookAuthors.DAO;
 using BookAuthors.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace BookAuthors.Controllers
 {
@@ -16,22 +17,50 @@ namespace BookAuthors.Controllers
         public IActionResult ViewBookAuthor(int authID)
         {
             BookAuthorModel found = _context.authors.FirstOrDefault(auth => auth.id == authID);
-            return View(found);
+            if(found != null)
+            {
+                return View(found);
+            } else 
+            {
+                ViewData["error"] = "No matching author found";
+                return View("Error");         
+            }
         }
         public IActionResult ListBookAuthors()
         {
             return View(_context);
         }
-        public IActionResult BookAuthorForm()
+        public IActionResult BookAuthorForm(BookAuthorModel newAuth)
         {
             return View("NewBookAuthor");
         }
         [HttpPost]
         public IActionResult AddBookAuthor(BookAuthorModel newAuth)
         {
+            if(ModelState.IsValid)
+            {
             _context.authors.Add(newAuth);
             _context.SaveChanges();
             return RedirectToAction("ListBookAuthors","BookAuthor");
+            } else 
+            {
+                List<string> errors = GetErrorListFromModelState(ModelState);
+                string displayErr = "";
+                errors.ForEach(err => displayErr += err);
+                ViewData["errors"] = displayErr;
+                return View("NewBookAuthor", newAuth);
+            }
+
+        }
+        // method to capture model state validation errors
+        public static List<string> GetErrorListFromModelState(ModelStateDictionary modelState)
+        {
+            IEnumerable<string> query = from state in modelState.Values
+                from error in state.Errors
+                select error.ErrorMessage;
+
+            List<string> errorList = query.ToList();
+            return errorList;
         }
     }   
 }
--- BookAuthors/Startup.cs
+++ BookAuthors/Startup.cs
@@ -55,7 +55,7 @@ namespace BookAuthors
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=BookAuthor}/{action=ListBookAuthors}/{id?}");
             });
         }
     }
